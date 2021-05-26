ac047ba6807e25f749251d1f039120847a25719a
Nasta <vasikkova.a@gmail.com>
2021-05-26 18:51:34 +0300

    Homwork04

--- modyl name/src/Main.java
+++ modyl name/src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        float n=9.5f;
-        float y=Math.round(n);
-        System.out.println("Result="+y);
+        //Дополнение к homework04
+        float n = 9.5f;
+        float y = Math.round(n);
+        System.out.println(Math.round(n));
+        System.out.println(Math.floor(n));
+        System.out.println(Math.ceil(n));
+
     }
-
 }
-
-
-
