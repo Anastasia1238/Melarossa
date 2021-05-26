cb1d7f3c21bb54307a6a2034fe8e979fbb1478de
Nasta <vasikkova.a@gmail.com>
2021-05-26 18:40:50 +0300

    Homworc03

--- modyl name/src/Main.java
+++ modyl name/src/Main.java
@@ -3,15 +3,9 @@ public class Main {
 
 
     public static void main(String[] args) {
-        int n = 12345;
-        int count;
-        int sum = 0;
-        while (n > 0) {
-            count = n % 10;
-            sum = sum + count;
-            n = n / 10;
-        }
-        System.out.println(sum);
+        float n=9.5f;
+        float y=Math.round(n);
+        System.out.println("Result="+y);
     }
 
 }
