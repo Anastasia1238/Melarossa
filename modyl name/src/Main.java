7d7373e2f12e19de8c69bb5eca40740ce0b369d0
Nasta <vasikkova.a@gmail.com>
2021-05-26 18:15:17 +0300

    Homworc02

---
+++ modyl name/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        int x = 10;
+        float y = 2;
+        double z = x / y;
+        System.out.println(z);
+
+        int oct = 10 % 3;
+        System.out.println(oct);
+
+        double c = Math.sqrt(x);
+        System.out.println(c);
+    }
+
+}
+
