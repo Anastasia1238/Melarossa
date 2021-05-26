28e4c7013891ed7c32be9fb9c41585f051db5315
Nasta <vasikkova.a@gmail.com>
2021-05-26 18:01:04 +0300

    Homworc01

--- modyl name/HomeWork/src/Main.java
+++ modyl name/HomeWork/src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Integer a=Integer.valueOf(args[0]);
+        Integer b=Integer.valueOf(args[1]);
+        Integer c=Integer.valueOf(args[2]);
+        Integer d=Integer.valueOf(args[3]);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+        Double g=4*Math.PI*(a*a/(b*(c+d)));
+        System.out.println(g);
     }
+
 }
+
