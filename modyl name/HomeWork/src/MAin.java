eb8068ea6acfc804e84198a90080b94234c0e2cd
Nasta <vasikkova.a@gmail.com>
2021-05-26 18:57:33 +0300

    Homwork05

---
+++ modyl name/HomeWork/src/MAin.java
@@ -0,0 +1,7 @@
+public class MAin {
+    public static void main(String[]args){
+        final int n=5;
+        double S=Math.PI*Math.pow(2,n);
+        System.out.println(S);
+    }
+}
--- modyl name/src/Main.java
+++ modyl name/src/Main.java
@@ -1,11 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        //Дополнение к homework04
-        float n = 9.5f;
-        float y = Math.round(n);
-        System.out.println(Math.round(n));
-        System.out.println(Math.floor(n));
-        System.out.println(Math.ceil(n));
+       final int n=5;
+
 
     }
 }
