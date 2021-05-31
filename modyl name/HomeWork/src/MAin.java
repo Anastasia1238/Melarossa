abbaf7bfa4161b8c91b8f5d9e6f40339e09f55fc
Nasta <vasikkova.a@gmail.com>
2021-05-31 17:50:50 +0300

    Weekday

--- modyl name/HomeWork/src/MAin.java
+++ modyl name/HomeWork/src/MAin.java
@@ -1,13 +1,37 @@
+import java.util.Scanner;
+
 public class MAin {
     public static void main(String[]args){
-       final int weght=45;
-       final int height=165;
 
-       int F=height-110;
-       if(weght<F){
-           System.out.println("Набрать вес:"+(F-weght));
-       }else {
-           System.out.println("Сбросить вес:"+(weght-F));
-       }
+        int weekDay=0;
+        Scanner in=new Scanner(System.in);
+        weekDay=in.nextInt();
+
+        switch (weekDay){
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+            default:
+        }
+        System.out.println("Ваш день недели:"+weekDay);
     }
+
+
 }
