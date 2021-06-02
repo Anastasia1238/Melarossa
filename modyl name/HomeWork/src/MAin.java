232fc8a1a8eec7aab2c33140d652effb0dcde5e2
Nasta <vasikkova.a@gmail.com>
2021-06-02 15:46:44 +0300

    Card

--- modyl name/HomeWork/src/MAin.java
+++ modyl name/HomeWork/src/MAin.java
@@ -1,9 +1,13 @@
-import java.util.Scanner;
-
 public class MAin {
     public static void main(String[] args) {
+        Card cr = new Card();
+        String crName = cr.getName();
+        long crNuber = cr.getNumber();
+        long crBulans = cr.getBalans();
 
-        int charRk=Character.getNumericValue('h');
-        System.out.println((int) 'h');
+        System.out.println("Имя владельца карты:" + crName);
+        System.out.println("Номеркарты карты:" + crNuber);
+        System.out.println("Баланс счета:" + crBulans);
+
     }
 }
