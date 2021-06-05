b30b57b8100fd634805e7708470719b67e36414e
Anastasia1238 <84405962+Anastasia1238@users.noreply.github.com>
2021-06-05 16:43:42 +0300

    111 (#12)
    * Homworc03
    
    * Homworc03
    
    * Homwork04
    
    * Homwork04
    
    * Homwork05
    
    * Homwork06
    
    * Homwork07
    
    * Weekday
    
    * Time
    
    * CyrillicLanguage
    
    * Card
    
    * Card1
    
    * Card1
    
    * CardHomework
    
    * 02.06.20

--- modyl name/src/Main.java
+++ modyl name/src/Main.java
@@ -1,18 +1,15 @@
 public class Main {
-
-
     public static void main(String[] args) {
-        int x = 10;
-        float y = 2;
-        double z = x / y;
-        System.out.println(z);
+        Card cr = new Card();
+        String crName = cr.getName();
+        long crNuber = cr.getNumber();
+        long crBulans = cr.getBalans();
 
-        int oct = 10 % 3;
-        System.out.println(oct);
+        System.out.println("Имя владельца карты:" + crName);
+        System.out.println("Номеркарты карты:" + crNuber);
+        System.out.println("Баланс счета:" + crBulans);
 
-        double c = Math.sqrt(x);
-        System.out.println(c);
     }
-
 }
+
 
