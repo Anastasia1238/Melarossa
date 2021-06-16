51cdb6db426902b85162da4de9486027079fd516
Nasta <vasikkova.a@gmail.com>
2021-06-16 14:27:16 +0300

    CARS

---
+++ Car/CARS/src/Engene.java
@@ -0,0 +1,33 @@
+public class Engene {
+    private String power;
+    private String producer;
+
+    public String getPower() {
+        return power;
+    }
+
+    public void setPower(String power) {
+        this.power = power;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public Engene(String power, String producer) {
+        this.power = power;
+        this.producer = producer;
+    }
+
+    @Override
+    public String toString() {
+        return "Engine{" +
+                "power='" + power + '\'' +
+                ", producer='" + producer + '\'' +
+                '}';
+    }
+}
